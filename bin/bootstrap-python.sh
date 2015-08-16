6fe7111150c9859976daf6d36901c1249ec570de
Daniel McDonald <mcdonadt@colorado.edu>
2015-08-15 23:01:10 -0600

    py27 and 34 good to go

---
+++ bin/bootstrap-python.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+curl -O miniconda.sh http://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh
+sh miniconda.sh -b
+
+echo "export PATH=${HOME}/miniconda3/bin:$PATH" >> ${HOME}/.bash_profile
+export PATH=${HOME}/miniconda3/bin:$PATH
+
+conda update --yes conda
+conda create --yes -n py34-base numpy scipy pandas matplotlib argcomplete jupyter
+conda create --yes -n py27-base python=2 numpy scipy pandas matplotlib argcomplete jupyter
+
+eval "$(register-python-argcomplete conda)"
+source activate py34-base
+
+echo "source activate py34-base" >> ${HOME}/.bash_profile
+
+ipython profile create
+
+echo "c.Application.verbose_crash=True" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.AliasManager.user_aliases = [" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "         ('ls', 'ls -lrthG')" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "         ]" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.InteractiveShellApp.pylab = 'auto'" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.TerminalIPythonApp.display_banner = False" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.TerminalInteractiveShell.colors = 'Linux'" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.TerminalInteractiveShell.autoindent = True" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.TerminalInteractiveShell.editor = 'vim'" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.TerminalInteractiveShell.confirm_exit = False" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.TerminalInteractiveShell.pager = 'less -FSR'" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.PromptManager.justify = False" >> ${HOME}/.ipython/profile_default/ipython_config.py
+echo "c.PromptManager.in_template = u'{color.Yellow}\T {color.White}({color.LightGreen}\\u{color.White}@{color.LightBlue}\h{color.White}):\Y1> '" >> ${HOME}/.ipython/profile_default/ipython_config.py
